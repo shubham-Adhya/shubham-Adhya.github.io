2355d1a9868c8c2e738151afd622ba72fe17b741
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-05-18 03:58:15 +0530

    stil in progress

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const portfolioArr=[
+  {
+    id:1,
+    image:IMG1,
+    title: "TripWise",
+    github: "https://github.com/shubham-Adhya/melodic-cream-5493",
+    demo: "https://inquisitive-heliotrope-8f69d2.netlify.app/"
+  },
+  {
+    id:2,
+    image:IMG2,
+    title: "TripWise",
+    github: "https://github.com/shubham-Adhya/melodic-cream-5493",
+    demo: "https://inquisitive-heliotrope-8f69d2.netlify.app/"
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {
+          portfolioArr.map(({id,image,title,github,demo})=>{
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank' rel='noreferrer'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const testimonialsArr=[
+  {
+    id:1,
+    avatar: AVTR1,
+    name: "John Snow",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium cum voluptatem reprehenderit fuga sit. Animi ad dolor ipsum fuga!"
+  },
+  {
+    id:2,
+    avatar: AVTR2,
+    name: "John Snow",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium cum voluptatem reprehenderit fuga sit. Animi ad dolor ipsum fuga!"
+  },
+  {
+    id:3,
+    avatar: AVTR3,
+    name: "John Snow",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium cum voluptatem reprehenderit fuga sit. Animi ad dolor ipsum fuga!"
+  },
+  {
+    id:4,
+    avatar: AVTR4,
+    name: "John Snow",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium cum voluptatem reprehenderit fuga sit. Animi ad dolor ipsum fuga!"
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonilas</h2>
+      <div className="container testimonials__container">
+        {
+          testimonialsArr.map(({id,avatar, name, review})=>{
+            return (
+              <article key={id} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt={name} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                  <small className='client__review'>
+                    {review}
+                  </small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
