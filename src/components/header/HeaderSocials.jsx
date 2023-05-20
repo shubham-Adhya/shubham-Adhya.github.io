e8cbc7c0d66a80b746b0e3cdbd725ccddfa8323d
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-05-20 17:23:51 +0530

    adding personal details

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
+
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
+
+
+const DarkTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.palette.common.black,
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.black,
+  },
+}));
+
+
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com/in/shubham-adhya" target='_blank'><BsLinkedin className='header__socials-icon'/></a>
-        <a href="https://github.com/shubham-Adhya" target='_blank'><FaGithub className='header__socials-icon'/></a>
+        <DarkTooltip title="LinkedIn" placement='right'>
+          <a href="https://linkedin.com/in/shubham-adhya" target='_blank' rel='noreferrer'>
+            <BsLinkedin className='header__socials-icon'/>
+          </a>
+        </DarkTooltip>
+
+        <DarkTooltip title="GitHub" placement='right'>
+        <a href="https://github.com/shubham-Adhya" target='_blank' rel='noreferrer'>
+          <FaGithub className='header__socials-icon'/>
+        </a>
+        </DarkTooltip>
+
         {/* <a href="https://linkedin.com/in/shubham-adhya" target='_blank'></a> */}
     </div>
   )
