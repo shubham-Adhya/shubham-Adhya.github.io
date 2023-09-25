6799c8d6f2eb28073ce8e08e12d31d30cc90fe91
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-09-25 19:52:16 +0530

    swagger skill

--- src/components/skill/BD.js
+++ src/components/skill/BD.js
@@ -2,6 +2,7 @@ import { FaNodeJs } from 'react-icons/fa'
 import { SiExpress } from 'react-icons/si'
 import { SiMongodb } from 'react-icons/si'
 import { SiPassport } from 'react-icons/si'
+import { SiSwagger } from 'react-icons/si'
 import { SiSocketdotio } from 'react-icons/si'
 import { GrMysql } from 'react-icons/gr'
 import { SiRedis } from 'react-icons/si'
@@ -24,6 +25,10 @@ const BD=[
     {
         icon: SiPassport,
         skillName: "OAuth"
+    },
+    {
+        icon: SiSwagger,
+        skillName: "Swagger"
     },
     {
         icon: SiSocketdotio,
